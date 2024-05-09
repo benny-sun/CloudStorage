f21a51bb77e127c8684f71d6b25a501ffee1f389
bennysun <bennysun.dev@gmail.com>
2024-05-09 20:45:58 +0800

    feat: add login feature

---
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/configs/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.udacity.jwdnd.c1.cloudstorage.configs;
+
+import com.udacity.jwdnd.c1.cloudstorage.services.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Autowired
+    public void registerProvider(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationService);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login", "/signup", "/css/**", "/js/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(formLogin -> formLogin
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler() {
+                    @Override
+                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+                        System.out.println("A user failed to login. Error: " + exception.getMessage());
+                        String redirectUrl = request.getContextPath() + "/login?error";
+                        response.sendRedirect(redirectUrl);
+                    }
+                })
+                .permitAll()
+            ).build();
+    }
+}
---
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/models/User.java
@@ -0,0 +1,67 @@
+package com.udacity.jwdnd.c1.cloudstorage.models;
+
+public class User {
+    private Integer userId;
+    private String username;
+    private String password;
+    private String salt;
+    private String firstName;
+    private String lastName;
+
+    public User(Integer userId, String username, String password, String salt, String firstName, String lastName) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.salt = salt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
