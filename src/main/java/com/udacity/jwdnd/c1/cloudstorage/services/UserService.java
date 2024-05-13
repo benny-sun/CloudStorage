9bf4550f63b46333c4933ef30aa0e58e31510d44
bennysun <bennysun.dev@gmail.com>
2024-05-13 20:48:42 +0800

    refactor: share dependent service

--- src/main/java/com/udacity/jwdnd/c1/cloudstorage/services/UserService.java
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/services/UserService.java
@@ -4,17 +4,16 @@ import com.udacity.jwdnd.c1.cloudstorage.models.User;
 import com.udacity.jwdnd.c1.cloudstorage.orm.UserMapper;
 import org.springframework.stereotype.Service;
 
-import java.security.SecureRandom;
-import java.util.Base64;
-
 @Service
 public class UserService {
     private final UserMapper userMapper;
     private final HashService hashService;
+    private final KeyService keyService;
 
-    public UserService(UserMapper userMapper, HashService hashService) {
+    public UserService(UserMapper userMapper, HashService hashService, KeyService keyService) {
         this.userMapper = userMapper;
         this.hashService = hashService;
+        this.keyService = keyService;
     }
 
     public boolean isUserExist(User user) {
@@ -26,10 +25,7 @@ public class UserService {
     }
 
     public int createUser(User user) {
-        SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
-        random.nextBytes(salt);
-        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String encodedSalt = keyService.generateSecureKey();
         String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
 
         return userMapper.insert(new User(null, user.getUsername(), hashedPassword, encodedSalt, user.getFirstName(), user.getLastName()));
