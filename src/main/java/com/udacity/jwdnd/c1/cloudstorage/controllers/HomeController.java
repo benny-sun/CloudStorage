099ec8d81324aa8244620748ff1d2f6f3370ff1d
bennysun <bennysun.dev@gmail.com>
2024-05-13 14:09:32 +0800

    feat: show notes

--- src/main/java/com/udacity/jwdnd/c1/cloudstorage/controllers/HomeController.java
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.udacity.jwdnd.c1.cloudstorage.controllers;
 
 import com.udacity.jwdnd.c1.cloudstorage.models.User;
+import com.udacity.jwdnd.c1.cloudstorage.orm.NoteMapper;
 import com.udacity.jwdnd.c1.cloudstorage.services.FileService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -13,15 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     private final FileService fileService;
+    private final NoteMapper noteMapper;
 
-    public HomeController(FileService fileService) {
+    public HomeController(FileService fileService, NoteMapper noteMapper) {
         this.fileService = fileService;
+        this.noteMapper = noteMapper;
     }
 
     @GetMapping
     public String showPage(Authentication auth, Model model) {
         User user = (User) auth.getPrincipal();
         model.addAttribute("files", fileService.getFiles(user.getUserId()));
+        model.addAttribute("notes", noteMapper.getNotes(user.getUserId()));
 
         return "home";
     }
--- src/main/java/com/udacity/jwdnd/c1/cloudstorage/orm/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/orm/NoteMapper.java
@@ -4,9 +4,16 @@ import com.udacity.jwdnd.c1.cloudstorage.models.Note;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface NoteMapper {
+
+    @Select("SELECT * FROM notes WHERE userid = #{userId}")
+    List<Note> getNotes(Integer userId);
+
     @Insert("Insert Into notes (notetitle, notedescription, userid) VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     int insert(Note note);
