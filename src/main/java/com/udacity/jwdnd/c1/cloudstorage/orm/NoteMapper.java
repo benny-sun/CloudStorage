9fdf908c80aac9f1492e9bf6c0c613bb5d2bfdd0
bennysun <bennysun.dev@gmail.com>
2024-05-13 15:49:42 +0800

    chore: uppercase sql statement

--- src/main/java/com/udacity/jwdnd/c1/cloudstorage/orm/NoteMapper.java
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/orm/NoteMapper.java
@@ -14,7 +14,7 @@ public interface NoteMapper {
     @Select("SELECT * FROM notes WHERE userid = #{userId}")
     List<Note> getNotes(Integer userId);
 
-    @Insert("Insert Into notes (notetitle, notedescription, userid) VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
+    @Insert("INSERT INTO notes (notetitle, notedescription, userid) VALUES (#{noteTitle}, #{noteDescription}, #{userId})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     int insert(Note note);
 }
