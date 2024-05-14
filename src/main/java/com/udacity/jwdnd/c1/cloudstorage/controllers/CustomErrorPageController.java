5d7af773cf1cbe9be43ea9d2c37297a16bc455ed
bennysun <bennysun.dev@gmail.com>
2024-05-14 17:17:36 +0800

    feat: handle error page

---
+++ src/main/java/com/udacity/jwdnd/c1/cloudstorage/controllers/CustomErrorPageController.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.c1.cloudstorage.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorPageController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+            HttpStatus httpStatus = HttpStatus.valueOf(statusCode);
+            model.addAttribute("title", statusCode + " " + httpStatus.getReasonPhrase());
+        }
+
+        return "error";
+    }
+}
